fa2af2f4b0544f7df66f4ca91aa4b6200432614a
lixinsu <sulixinict@gmail.com>
2019-01-04 00:07:34 +0800

    Add miscellaneous

---
+++ train_squad_probe.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export SQUAD_DIR=/users/sulixin/research/sigir/pytorch-pretrained-BERT/data/datasets/squad
+
+python3 examples/run_squad.py \
+  --insert_probe \
+  --bert_model bert-base-uncased \
+  --do_predict \
+  --do_train \
+  --train_file $SQUAD_DIR/train-v1.1-rc.json \
+  --predict_file $SQUAD_DIR/dev-v1.1.json \
+  --train_batch_size 24 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 2.0 \
+  --max_seq_length 384 \
+  --doc_stride 128 \
+  --output_dir data/models/squadv1.1_b24_truncated_trainprobe/ \
+  #--debug
+
