e15294dd4de033591c08da846c35112d3839b7a3
lixinsu <sulixinict@gmail.com>
2019-01-14 10:50:05 +0800

    Use relative path for checkpoint

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -37,14 +37,14 @@ from .file_utils import cached_path
 logger = logging.getLogger(__name__)
 
 PRETRAINED_MODEL_ARCHIVE_MAP = {
-    'bert-base-uncased': '/users/sulixin/research/sigir/pytorch-pretrained-BERT/data/downloads/bert-base-uncased.tar.gz',
+    'bert-base-uncased': 'data/downloads/bert-base-uncased.tar.gz',
      #"https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased.tar.gz",
     'bert-large-uncased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-large-uncased.tar.gz",
     'bert-base-cased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-cased.tar.gz",
     'bert-large-cased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-large-cased.tar.gz",
     'bert-base-multilingual-uncased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-uncased.tar.gz",
     'bert-base-multilingual-cased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-cased.tar.gz",
-    'bert-base-chinese': '/users/sulixin/research/sigir/pytorch-pretrained-BERT/data/downloads/bert-base-chinese.tar.gz',
+    'bert-base-chinese': 'data/downloads/bert-base-chinese.tar.gz',
     #'bert-base-chinese': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-chinese.tar.gz",
 }
 CONFIG_NAME = 'bert_config.json'
--- pytorch_pretrained_bert/tokenization.py
+++ pytorch_pretrained_bert/tokenization.py
@@ -28,14 +28,14 @@ from .file_utils import cached_path
 logger = logging.getLogger(__name__)
 
 PRETRAINED_VOCAB_ARCHIVE_MAP = {
-    'bert-base-uncased': '/users/sulixin/research/sigir/pytorch-pretrained-BERT/data/downloads/bert-base-uncased-vocab.txt',
+    'bert-base-uncased': 'data/downloads/bert-base-uncased-vocab.txt',
     # "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-vocab.txt",
     'bert-large-uncased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-large-uncased-vocab.txt",
     'bert-base-cased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-cased-vocab.txt",
     'bert-large-cased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-large-cased-vocab.txt",
     'bert-base-multilingual-uncased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-uncased-vocab.txt",
     'bert-base-multilingual-cased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-cased-vocab.txt",
-    'bert-base-chinese': "/users/sulixin/research/sigir/pytorch-pretrained-BERT/data/downloads/bert-base-chinese-vocab.txt",
+    'bert-base-chinese': "data/downloads/bert-base-chinese-vocab.txt",
     #'bert-base-chinese': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-chinese-vocab.txt",
 }
 PRETRAINED_VOCAB_POSITIONAL_EMBEDDINGS_SIZE_MAP = {
